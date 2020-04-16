24775e3499a6d5b874149b3e62603527fbb6813a
Adrián Núñez <anunezpedroche@gmail.com>
2020-04-16 19:04:13 +0200

    Primera subida, Login creado

---
+++ server.js
@@ -0,0 +1,43 @@
+const express      = require("express");
+const path         = require("path");
+const cors         = require("cors");
+const passport     = require('passport');
+const session      = require('express-session');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+// Middleware for passport
+require('./config/passport');
+
+// Middlewares
+app.use(express.json());
+app.use(cors({credentials: true, origin: 'http://localhost:3001'}));
+app.use(cookieParser());
+
+// Para enviar un FORM a traves de req. tal.
+// Para que solo puedas pasar archivos texto plano 
+// a traves de la URL. Gracias Carlos.
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({ 
+    secret: 'secretKey',
+    resave: false,
+    saveUninitialized: false 
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Paginas publicas (estaticas)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Require Users routes
+app.use("/api/users", require("./app/routes/users.routes.js"));
+/*app.use("/api/techs", require("./app/routes/techs.routes.js"));
+app.use("/api/projects", require("./app/routes/projects.routes.js"));
+app.use("/api/reports", require("./app/routes/reports.routes.js"));*/
+
+
+app.listen(3000, () => {
+    console.log(" * Servidor corriendo en: http://localhost:3000");
+});
---
+++ src/Redux/Reducers/index.js
@@ -0,0 +1,7 @@
+import {combineReducers} from 'redux';
+import UserReducer from './UserReducer.js';
+
+export default combineReducers({
+    UserReducer
+})
+
